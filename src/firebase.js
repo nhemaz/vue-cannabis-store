2b29f9050161c885b3105ddf20e990df51738db8
Cliff.io <40029661+nhemaz@users.noreply.github.com>
2021-06-02 19:00:51 +0200

    Update Firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/firebase-auth'
 import 'firebase/storage'
   // Initialize Firebase
   var config = {
-     apiKey: "AIzaSyBzwhFoBLcMGLeTLdlH_mgKc3o6-eDZNcQ",
+     apiKey: "#########################################",  //Your API Key//
     authDomain: "gfarmshop.firebaseapp.com",
     databaseURL: "https://gfarmshop.firebaseio.com",
     projectId: "gfarmshop",
     storageBucket: "gfarmshop.appspot.com",
-    messagingSenderId: "881319607161",
-    appId: "1:881319607161:web:e092dde154b76150b4cda8",
+    messagingSenderId: "############",
+    appId: "1:############:web:#################",
   };
   
 const fb = firebase.initializeApp(config);
